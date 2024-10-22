648e52ff1fcc040035716c18e5561bf74781ccdd
QC2168 <tom.huang.2002@gmail.com>
2024-10-22 21:51:13 +0800

    feat: loginByMobile

--- app/controller/users.js
+++ app/controller/users.js
@@ -1,15 +1,22 @@
 const Controller = require('egg').Controller;
 class UsersController extends Controller {
-  async create() {
-    const { ctx } = this;
-    ctx.model.Users.create({
-      username: '_island',
-      email: 'example@example.com',
-      password: 'h9nUQ92B'
-    });
-    ctx.body = {
-      message: '创建成功'
-    };
+  async loginByMobile() {
+    const ctx = this.ctx;
+    const { mobile, code } = ctx.request.body;
+    const user = await ctx.service.users.checkUserByMobile({ mobile, code });
+    if (user === 'forbidden') {
+      ctx.body = this.ctx.resultErrorData('用户已被限制使用，无法登录', null);
+      ctx.status = 404;
+    } else if (user) {
+      ctx.body = this.ctx.resultData('登录成功', {
+        accessToken: this.ctx.helper.generateToken({ id: user.id }),
+        info: user,
+      });
+      ctx.status = 200;
+    } else {
+      ctx.body = this.ctx.resultErrorData('验证码有误，请重试');
+      ctx.status = 404;
+    }
   }
 }
 module.exports = UsersController;
